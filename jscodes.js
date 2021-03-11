f98c8ec743811dda60d7dcc1da3208ad79f4cab3
adedolapo <shayne.doly94@gmail.com>
2021-03-11 09:37:45 +0100

    includes arithmetic operations

--- jscodes.js
+++ jscodes.js
@@ -12,3 +12,18 @@ console.log(typeof name);
 console.log(typeof constantLight);
 console.log(typeof toDo);
 
+//using arithmetic operators
+let figure1 = 10;
+let figure2 = 23;
+const sum = figure1 + figure2;
+const multiply = figure1 * figure2;
+const divide = figure2 / figure1;
+
+const newfig1 = figure1++; //unary operators
+const newfig2 = figure2--;
+
+//using the short hand arithmetic operators +=, -=
+figure1 += 12;
+figure2 -= 3;
+console.log(figure1, figure2);
+
