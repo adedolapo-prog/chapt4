dcbde2aae0af6c68286ea0e7fbfe4bb931ddb500
adedolapo <shayne.doly94@gmail.com>
2021-03-11 09:28:44 +0100

    inputs the different data types

---
+++ jscodes.js
@@ -0,0 +1,14 @@
+//declaring variables
+
+const population = 100; //this is a numeric variable
+const name = "My name is Adedolapo"; //this is a string
+const constantLight = false; //this is a boolean variable
+const toDo = null; //this is a null set
+const rareFactor; //this is an undefined variable
+
+//to check the data type
+console.log(typeof population);
+console.log(typeof name);
+console.log(typeof constantLight);
+console.log(typeof toDo);
+
