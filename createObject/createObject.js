baf1bda13c7b2497cc7256a32159d51e1a1ab3a8
adedolapo <shayne.doly94@gmail.com>
2021-03-15 23:00:00 +0100

    adds new objects assignment folder

---
+++ createObject/createObject.js
@@ -0,0 +1,65 @@
+//3 realistic objects with at least 10 key value pairs
+
+const building = {
+	storey: 2,
+	color: 'light blue',
+	architect: 'Mademan Quantity surveyors',
+	location: 'Lagos',
+	roofing: 'zinc';
+	numberOfBedrooms: 6,
+	numberOfBathrooms: 12,
+	lighting: 'neon-white',
+	furnishing: {
+		cushion: 'hollow-fill fiber',
+		curtain:'pinch pleat',
+		Rugs: 'jute and bamboo',
+		chairCovering: 'clothes',
+		floorCovering: 'Tiles',
+	}
+	buildingType: 'duplex',
+}
+
+const teacher = {
+	name: 'Eben Don',
+	occupation: 'backend developer',
+	stateOfOrigin: 'Kogi state',
+	nationality: 'Canadian',
+	spouseName: 'Shukurah',
+	monthlyIncome: 'One million naira',
+	hobbies: ['singing', 'swimming', 'reading'],
+	countryOfResidence: 'Canada',
+	carOwned: {
+		brand: 'Chevrolet',
+		model: 'camaro'
+		productionYear: 2013,
+		yearsOfUsage: 4,
+		license: 'xyz 2012 uz',
+	}
+	favoriteQuote: 'I don\'t ever give up',
+}
+
+const grammy = {
+	type: 'award',
+	foundingYear: 1959,
+	latestEdition: 2021,
+	numberOfPerformingArtistes: 30,
+	nominationType: 'categorical',
+	nominationsPerCategory: 5,
+	totalNumberOfWinners: 84,
+	funFacts: {
+		mostNominationsWithoutWins: 'Snoop Dogg',
+		oldestGrammyWinner: 'Pinetop Perkins',
+		youngestGrammyWinner: 'LeAnn Rimes',
+		mostOneNightWins: 'Michael Jackson',
+	}
+}
+
+
+//3 realistic arrays with atleast 3 other arrays as elements
+
+const materialItems = [['benz', 'honda', 'chevrolet'], ['rolex', 'chopard', 'blancpain'], ['oxford', 'loafers', 'brogues']];
+
+const furnitures = [['wing chair', 'deck chair', 'sofa'], ['round table', 'desk', 'credence table'], ['phenolic locker', 'laminate locker', 'wooden locker']];
+
+const locations = [['Canada', 'USA', 'UK'], ['Mount Kilimanjaro', 'Everest', 'Mount Fuji'], ['lake Baikal', 'lake Michigan', 'lake Huron']];
+
